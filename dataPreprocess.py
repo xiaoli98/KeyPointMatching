a2340f054d6fa454d3f1e520525c03fcfded597d
Paolo Fasano <super-pablo@hotmail.it>
2022-09-26 19:10:29 +0200

    data preprocessing added new preprocessing method

--- dataPreprocess.py
+++ dataPreprocess.py
@@ -1,4 +1,8 @@
+from sqlite3 import Row
 from track_1_kp_matching import *
+
+import os
+import pandas as pd
 
 #TODO
 #la tokenizzazione si fa dopo aver creato il dataset
@@ -9,102 +13,181 @@ from track_1_kp_matching import *
 
 
 class Argument:
+
+    def __init__(self) -> None:
+        self.__argId = None
+        self.__argument = None
+        self.__topic = None
+        self.__stance = None
+
     @property
     def argId(self):
-        return self.argId
+        return self.__argId
     
     @property
     def argument(self):
-        return self.argument
+        return self.__argument
     
     @property
     def topic(self):
-        return self.topic
+        return self.__topic
     
     @property
     def stance(self):
-        return self.stance
+        return self.__stance
     
     @argId.setter
     def argId(self, argId):
-        self.argId = argId
+        self.__argId = argId
         
     @argument.setter
     def argument(self, argument):
-        self.argument = argument
+        self.__argument = argument
         
     @topic.setter
     def topic(self, topic):
-        self.topic = topic
+        self.__topic = topic
         
     @stance.setter
     def stance(self, stance):
-        self.stance = stance
+        self.__stance = stance
         
 
 class KeyPoint:
+    
+    def __init(self) -> None:
+        self.__keyPointId = None 
+        self.__key_point = None 
+        self.__topic = None 
+        self.__stance = None 
+    
     @property
-    def KeyPointId(self):
-        return self.KeyPointId
+    def keyPointId(self):
+        return self.__keyPointId
     
     @property
     def key_point(self):
-        return self.key_point
+        return self.__key_point
     
     @property
     def topic(self):
-        return self.topic
+        return self.__topic
     
     @property
     def stance(self):
-        return self.stance
+        return self.__stance
     
-    @KeyPointId.setter
-    def KeyPointId(self, KeyPointId):
-        self.KeyPointId = KeyPointId
+    @keyPointId.setter
+    def keyPointId(self, keyPointId):
+        self.__keyPointId = keyPointId
         
     @key_point.setter
     def key_point(self, key_point):
-        self.key_point = key_point
+        self.__key_point = key_point
         
     @topic.setter
     def topic(self, topic):
-        self.topic = topic
+        self.__topic = topic
         
     @stance.setter
     def stance(self, stance):
-        self.stance = stance
+        self.__stance = stance
         
 
-class Label:
+class Label():
+
+    def __init__(self):
+        self.__argId = None
+        self.__keyPointId = None
+        self.__label = None
+        
     @property
-    def KeyPointId(self):
-        return self.KeyPointId
+    def keyPointId(self):      
+        return self.__keyPointId
+        
     
     @property
     def argId(self):
-        return self.argId
+        return self.__argId
     
     @property
     def label(self):
-        return self.label
+        return self.__label
     
-    
-    @KeyPointId.setter
-    def KeyPointId(self, KeyPointId):
-        self.KeyPointId = KeyPointId
+    @keyPointId.setter
+    def keyPointId(self, keyPointId):
+        self.__keyPointId = keyPointId
+        
         
     @argId.setter
     def argId(self, argId):
-        self.argId = argId
+        self.__argId = argId
         
     @label.setter
     def label(self, label):
-        self.label = label
+        self.__label = label
         
 
 def preprocess(path="kpm_data", subset="train"):
-    arguments_df, key_points_df, labels_file_df = load_kpm_data(path, subset)
+    arguments_df, key_points_df, labels_file_df = readCSV(path, subset)#load_kpm_data(path, subset)
+
+    labels = []
+    for _, row in labels_file_df.iterrows():
+        l = Label()
+        l.keyPointId = row["key_point_id"]
+        l.argId = row["arg_id"]
+        l.label = row["label"]
+        labels.append(l)
+
+    arguments = {}
+    for _, row in arguments_df.iterrows():
+        arg = Argument()
+        arg.argId = row["arg_id"]
+        arg.argument = row["argument"]
+        arg.topic = row["topic"]
+        arg.stance = row["stance"]
+        arguments[arg.argId] = arg 
+   
+    keyPoints = {}
+    for _, row in key_points_df.iterrows():
+       kp = KeyPoint()
+       kp.keyPointId = row['key_point_id']
+       kp.key_point = row['key_point']
+       kp.topic = row['topic']
+       kp.stance = row['stance']
+       keyPoints[kp.keyPointId] = kp
+        
     
+    processedData = pd.DataFrame()
+    
+    data = []
+    for label in labels:
+        argId = label.argId
+        keyId = label.keyPointId
+        if arguments[argId].stance == keyPoints[keyId].stance:
+            data.append([
+                argId, keyId, [arguments[argId].argument,  keyPoints[keyId].key_point], label.label
+            ])
+    
+    #TODO tokenize data[2]
+    return data
+    
+def readCSV(path, subset):
+    arguments_file = os.path.join(path, f"arguments_{subset}.csv")
+    key_points_file = os.path.join(path, f"key_points_{subset}.csv")
+    labels_file = os.path.join(path, f"labels_{subset}.csv")
+       
+    arguments_df = pd.read_csv(arguments_file)
+    key_points_df = pd.read_csv(key_points_file)
+    labels_file_df = pd.read_csv(labels_file)
+
+    return arguments_df, key_points_df, labels_file_df
+
+def getRow(df, idToSearch, cl_name):
+    for index, row in df.iterrows():
+        if(row[cl_name] == idToSearch):
+            return row
+    return -1
     
 
+preprocess()
